805c33906fc61497210a5831cdb31aaf4001e1fd
Clear <576839360@qq.com>
2016-08-29 11:14:41 +0800

    ✨ just new function find

--- index.js
+++ index.js
@@ -8,4 +8,5 @@ module.exports = {
     camelize: require('./packages/camelize'),
     extend: require('./packages/extend-it'),
     type: require('./packages/type'),
+    find: require('./packages/find'),
 };
