74b671b73b4c70686d8de09a178cc8bc50c027be
Clear <576839360@qq.com>
2016-08-31 13:51:06 +0800

    ⬆️ just update `getQuery()`

--- packages/get-query/index.js
+++ packages/get-query/index.js
@@ -2,11 +2,18 @@
  * get url query
  *
  * @param {String} name
+ * @param {String} search
  */
 
-function getQuery(name) {
+function getQuery(name, search) {
+    if (!search) {
+        return null;
+    }
+    while (search && search[0] === '?') {
+        search = search.substr(1);
+    }
     var queryRegExp = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i'),
-        queryString = location.search.substr(1).match(queryRegExp);
+        queryString = search.match(queryRegExp);
     if(queryString != null) {
         return unescape(queryString[2]);
     }
